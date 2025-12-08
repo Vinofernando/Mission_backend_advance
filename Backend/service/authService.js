4d5d0152fd7fb4171661cdf17c8bacdca1ca520b
Vino <vinofernando84@gmail.com>
2025-12-08 18:06:22 +0700

    add login service

--- Backend/service/authService.js
+++ Backend/service/authService.js
@@ -2,6 +2,9 @@ import pool from "../config/db.js"
 import bcrypt from "bcrypt"
 import { v4 as uuidv4 } from 'uuid'
 import { sendEmailVerification } from "./emailService.js"
+import jwt from "jsonwebtoken"
+
+const JWT_SECRET = process.env.JWT_SECRET
 
 export const registerUser = async({fullname, username, email, password}) => {
     if(!fullname, !username, !email, !password) throw ({status: 400, message: `All field required`})
@@ -24,4 +27,31 @@ export const registerUser = async({fullname, username, email, password}) => {
     await sendEmailVerification(email, verifyToken)
 
     return  {message : "Your email registred, check your email for verification"}
+}
+
+export const loginUser = async({email, password}) => {
+    if(!email || !password) throw ({status: 400, message: "All field required"})
+
+    const result = await pool.query(
+        `SELECT * FROM users WHERE email = $1`, [email]
+    )
+
+    if(result.rows.length === 0) throw ({status: 400, message: "Email has not resgister or verify"})
+
+    const user = result.rows[0]
+    const validPassword = await bcrypt.compare(password, user.password)
+
+    if(!validPassword) throw ({status: 400, message: "Wrong password"})
+
+    const token = jwt.sign({id: user.user_id, username: user.username, email: user.email}, JWT_SECRET, {expiresIn: "1h"})
+
+    return({
+        message: "Berhasil login",
+        token,
+        user: {
+            id: user.user_id,
+            username: user.username,
+            email: user.email
+        }
+    })
 }
