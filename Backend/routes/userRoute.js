8b416d7945c87278d367f6510d5288b4e92a46e0
Vino <vinofernando84@gmail.com>
2025-12-08 21:24:47 +0700

    add upload route

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,9 +1,12 @@
 import express from "express"
 import { user } from "../controller/userController.js";
 import { authenticateToken } from "../middleware/auth.js"
+import { uploadFile } from "../controller/uploadController.js";
+import { upload } from "../utility/uploadFileHandler.js";
 
 const router = express.Router()
 
 router.get("/", authenticateToken, user)
+router.post("/upload",authenticateToken, upload.single('image'), uploadFile)
 
 export default router
