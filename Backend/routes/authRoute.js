2b201eb29b84183273a57386045dba2ac14da402
Vino <vinofernando84@gmail.com>
2025-12-08 18:07:09 +0700

    add login route

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { register } from '../controller/authController.js'
+import { register, login } from '../controller/authController.js'
 import { verifyAccount } from '../controller/verifyController.js'
 
 const router = express.Router()
 
 router.post("/register", register)
+router.post("/login", login)
 router.get("/verify/:token", verifyAccount)
 
 export default router
